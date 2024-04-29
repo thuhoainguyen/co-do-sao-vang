c572688073dd81e21361912c14268a13c000a3c4
thuhoainguyen <thunguyen.vina@gmail.com>
2024-04-29 15:20:32 +0200

    update ex2

--- test.py
+++ test.py
@@ -1,4 +1,5 @@
 # Lesson_1: Variables and String Format
+print("Lesson 1: ")
 print("Welcome to Python class")
 
 num_1 = 1.2
@@ -8,7 +9,10 @@ print("Sum of", num_1, "and", num_2, "is", num_1 + num_2)
 sum = num_1 + num_2
 print(f"The sum of {num_1} and {num_2} is {sum}")
 
+print("-----------------------------------------------")
+
 # Lesson_2: Data types
+print("Lesson 2: Data types")
 var_integer = 2
 var_float = 5.5
 var_string = "cafe"
@@ -23,26 +27,44 @@ print(f"Size of the list: {len(var_list)}")
 var_dict = {"name": "AnhTu", "age": 27, "is_student": False}
 print(f"Name: {var_dict['name']}, age: {var_dict['age']}, is student: {var_dict['is_student']}")
 
+print("-----------------------------------------------")
+
 # Lesson_3: Conditional Statements 
+print("Lesson 3: Conditional Statements")
 
 # Ex.1: Write a program that asks the user to enter a number and prints out whether it is even or odd. 
 # (Assume that the user can only enter integers)
+print("Ex1: Even/Odd number")
 
-num = int(input("Enter a number: "))
+prompt = "Enter a number: "
+# num = int(input(prompt))
+num = 6
 if num % 2 == 0:
     print("Even")
 else:
     print("Odd")
+
+print("-----------------------------------------------")
 
 # Ex.2: Write a program that simulates a traffic light sequence based on a given signal.
 # The valid inputs are 'red', 'yellow', and 'green'. The program should print out the next signal.
 # For example, if the user enters 'red', the program should print out 'green'.
 # If input is not valid, the program should print out 'invalid input'.
 
-# input("Enter a traffic signal (green, yellow, red):")
-# if input == "green":
-#     print("yellow")
-# elif input == ""
+print("Ex2: ")
+
+light = input("Enter a traffic signal (green, yellow, red): ")
+if light == "green":
+    print("yellow")
+elif light == "yellow":
+    print("red")
+elif light == "red":
+    print("green")
+else:
+    print("invalid input")
+    
+
+
 
 
 
